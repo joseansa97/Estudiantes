7813f8cacf2dc95c71fc8fb73a8079241f5621c5
Jose Antonio <josesito@gmail.com>
2019-04-12 11:39:20 -0500

    Hola Mundo

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
-<?php
-require 'crud/sitemap.php';
-require 'config/header.php';
-require 'crud/nav.php';
-require $contenido;
-require 'config/footer.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hola Mundo</title>
+    <style>
+        .hola{
+            width: 500px;
+            height: 200px;
+            margin: 0 auto;
+            background: skyblue;
+            border: 1px solid black;
+        }
 
-?>
+        h1, h2 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="hola">
+        <h1>Hola Mundo</h1>
+        <h2>Microsoft Azure</h2>
+    </div>
+</body>
+</html>
